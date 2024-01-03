9e0333e9aabcc0b7dd67c6acd872ad48d6ae679d
Leon <jch.lion97@gmail.com>
2024-01-03 14:12:24 -0500

    still fixing

--- api/src/server.js
+++ api/src/server.js
@@ -7,7 +7,7 @@ const server = express();
 server.use(express.json());
 server.use(
   cors({
-    origin: ["https://dogs-api-isdv.onrender.com"],
+    origin: ["https://dogs-api-isdv.onrender.com", "http://localhost:5173"],
     methods: ["GET", "POST"],
   })
 );
