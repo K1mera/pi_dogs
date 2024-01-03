aa015391e3c730b51a68e3f2a8c540d9e2c90419
Leon <jch.lion97@gmail.com>
2024-01-03 14:07:44 -0500

    db route changed

--- api/src/db.js
+++ api/src/db.js
@@ -4,10 +4,10 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_ROUTE, DB_DEPLOY, API_KEY,  } =
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_ROUTE, API_KEY } =
   process.env;
 
 export const sequelize = new Sequelize(
-  `postgres://${ DB_DEPLOY }?apikey=${API_KEY}`,
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_ROUTE}?apikey=${API_KEY}`,
   { logging: false }
 );
