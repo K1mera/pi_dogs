eda911636c99883f641b25a781fcc89e1d4436cf
Leon <jch.lion97@gmail.com>
2024-01-05 17:18:48 -0500

    re-try again

--- api/src/server.js
+++ api/src/server.js
@@ -3,7 +3,7 @@ import dogsRoutes from './routes/server.routes.js'
 import cors from 'cors'
 
 const server = express();
-
+server.options("*", cors());
 server.use(express.json());
 server.use(
   cors({
