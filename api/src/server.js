afdcea117d6bae35919789d31339074965dca8ac
Leon <jch.lion97@gmail.com>
2024-01-07 17:39:34 -0500

    fixing cors

--- api/src/server.js
+++ api/src/server.js
@@ -6,7 +6,17 @@ const server = express();
 server.options("*", cors());
 server.use(express.json());
 server.use(
-  cors()
+  cors(
+    {
+    origin: [
+      "https://dogs-api-isdv.onrender.com",
+      "http://localhost:5173",
+      "https://659882d91b14f060e1b8dace--eclectic-monstera-e53b46.netlify.app",
+      "https://659882d91b14f060e1b8dace--eclectic-monstera-e53b46.netlify.app/home",
+    ],
+    methods: ["GET", "POST"],
+  }
+  )
 );
 
 server.use(dogsRoutes)
