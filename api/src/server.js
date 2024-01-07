c5e3c55792ce48b09b3972c3daa755a46e1abc1e
Leon <jch.lion97@gmail.com>
2024-01-07 18:02:25 -0500

    fixing cors again

--- api/src/server.js
+++ api/src/server.js
@@ -7,15 +7,15 @@ server.options("*", cors());
 server.use(express.json());
 server.use(
   cors(
-    {
-    origin: [
-      "https://dogs-api-isdv.onrender.com",
-      "http://localhost:5173",
-      "https://659882d91b14f060e1b8dace--eclectic-monstera-e53b46.netlify.app",
-      "https://659882d91b14f060e1b8dace--eclectic-monstera-e53b46.netlify.app/home",
-    ],
-    methods: ["GET", "POST"],
-  }
+  //   {
+  //   origin: [
+  //     "https://dogs-api-isdv.onrender.com",
+  //     "http://localhost:5173",
+  //     "https://659882d91b14f060e1b8dace--eclectic-monstera-e53b46.netlify.app",
+  //     "https://659882d91b14f060e1b8dace--eclectic-monstera-e53b46.netlify.app/home",
+  //   ],
+  //   methods: ["GET", "POST"],
+  // }
   )
 );
 
